4cc108deb338aaa4239f31bc0dfe7bf3bca9b140
sami <sami@samis-MacBook-Pro.local>
2021-06-08 14:14:51 +0300

    first commit

---
+++ Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_with_ASP.NET.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Required(ErrorMessage = "It is required field!!")]
+        public string Description { get; set; }
+        [Required(ErrorMessage= "Please, insert a invalid value here ^_^")]
+        public float Price { get; set; }
+    }
+}
